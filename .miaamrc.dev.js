740075ab3d93b597a26bc5984f419ff68d434892
Noman5237 <anonyman637@gmail.com>
2022-02-23 03:15:04 +0600

    sample updated

--- .miaamrc.dev.js
+++ .miaamrc.dev.js
@@ -2,14 +2,13 @@ module.exports = {
 	mode: 'development',
 	paths: {
 		public: './public',
-		assets: './public/assets',
-		src: './src/',
+		assets: './public/asset',
+		src: './src',
 	},
 	index: './src/index.js',
 	server: {
 		port: 3007,
 	},
-	watch: true,
 	watchOptions: {
 		ignored: '/node_modules/',
 	},
--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-// import * as PIXI from 'pixi.js';
-// import { Sprite } from 'pixi.js';
-// import treeTexture from '../public/assets/tree.texture.jpg';
-
-// console.log('aaa', treeTexture);
-// // Create app
-// const app = new PIXI.Application({
-// 	antialias: true,
-// 	autoDensity: true,
-// 	backgroundColor: 0x1099bb,
-// 	resolution: devicePixelRatio,
-// });
-// const sprite = new Sprite(treeTexture);
-// app.stage.addChild(sprite);
-// document.body.appendChild(app.view);
-
-console.log('hello');
+console.log('hiiia');
