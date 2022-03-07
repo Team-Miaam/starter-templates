7edfa60430d78d4017fd111d49df7e2988722bfc
Noman5237 <anonyman637@gmail.com>
2022-03-08 00:14:38 +0600

    miaam starter update

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	env: {
+		browser: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		sourceType: 'module',
+		babelOptions: {
+			configFile: './node_modules/miaam-scripts/configs/babelrc.json',
+		},
+	},
+	plugins: ['@babel', 'import', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		// printWidth rule in prettier is not working
+		'max-len': ['error', { code: 120, tabWidth: 2 }],
+		'import/extensions': ['error', 'always'],
+		'import/no-absolute-path': 0,
+	},
+	globals: {
+		__createChunk__: 'readonly',
+	},
+};
---
+++ .miaamrc.dev.js
@@ -0,0 +1,16 @@
+module.exports = {
+	mode: 'development',
+	paths: {
+		assets: './assets',
+		src: './src',
+		dist: './dist/',
+		build: './build',
+	},
+	index: './src/index.mjs',
+	server: {
+		port: 3007,
+	},
+	watchOptions: {
+		ignored: ['/node_modules/'],
+	},
+};
