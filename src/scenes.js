92a924af3f596f065f2b07bb744999363583422a
Noman5237 <anonyman637@gmail.com>
2022-03-08 00:01:14 +0600

    updated miaam starter template

--- src/scenes.js
+++
@@ -1,6 +0,0 @@
-const scenes = {
-	one: __createChunk__('/src/scenes/one.scene.js'),
-	two: __createChunk__('/src/scenes/two.scene.js'),
-};
-
-export default scenes;
--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { describe, test } from 'mocha';
-
-describe('Simple Test', () => {
-	test('Hello', () => {
-		expect(2 + 2).to.equals(4);
-	});
-});
